41b23e5725d332c31cc7638e58e6382ea594ea05
kks <kks@host.docker.internal>
2023-02-11 12:48:48 +0900

    스프링 컨테이너에 직접 빈등록에서 빈팩토리를 등록해 객체 생성과 조립을 위임

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,14 +2,29 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class HellobootApplication {
-	@SuppressWarnings("resource")
+	
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+	
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
+	
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -22,9 +37,9 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
+		applicationContext.register(HellobootApplication.class);
+//		applicationContext.registerBean(SimpleHelloService.class);
 		
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
 	}
 }
