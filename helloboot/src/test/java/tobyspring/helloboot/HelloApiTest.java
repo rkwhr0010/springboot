fd9ca799be60723c04f474ca2ce7879bc0cb91b8
kks <kks@host.docker.internal>
2023-02-11 16:50:26 +0900

    컨트롤러 실패케이스 추가

--- helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -22,9 +22,17 @@ public class HelloApiTest {
 		assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
 		//body Hello Spring
 		assertThat(res.getBody()).isEqualTo("Hello Spring");
+	}
+	
+	@Test
+	void failHelloApi() {
+		TestRestTemplate rest = new TestRestTemplate();
 		
+		ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
+	
 }
 
 
