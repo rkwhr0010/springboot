6a083bb5a88d91ea12e807d75eba9ccd9e2ae474
kks <kks@host.docker.internal>
2023-02-11 12:29:37 +0900

    20230211-프론트컨트롤러구현을 디스패처서블릿으로 변경

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,6 +2,11 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/hello")
 public class HelloController {
 	
 	private final HelloService helloService;
@@ -10,7 +15,9 @@ public class HelloController {
 		super();
 		this.helloService = helloService;
 	}
-
+	
+	@GetMapping
+	@ResponseBody
 	public String hello(String name) {
 		return  helloService.sayHello(Objects.requireNonNull(name));
 	}
