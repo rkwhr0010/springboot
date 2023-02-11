15327cfacc8e3c667ba8308fc60180c087965aac
kks <kks@host.docker.internal>
2023-02-11 13:00:33 +0900

    .

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,7 +3,6 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
