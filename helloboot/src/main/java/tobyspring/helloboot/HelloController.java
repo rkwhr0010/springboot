eee43e66fffac9dfa46179264f9781623f93fe0c
kks <kks@host.docker.internal>
2023-02-11 15:22:47 +0900

    .

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,6 +2,7 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	
 	private final HelloService helloService;
+	private final ApplicationContext applicationContext;
 	
-	public HelloController(HelloService helloService) {
+	
+	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
 		super();
+		this.applicationContext = applicationContext;
 		this.helloService = helloService;
 	}
 	
@@ -19,4 +23,5 @@ public class HelloController {
 	public String hello(String name) {
 		return  helloService.sayHello(Objects.requireNonNull(name));
 	}
+
 }
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,8 +1,10 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -11,23 +13,17 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan //현재 클래스 경로 기준으로 하위 경로 탐색
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 	
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-				WebServer webServer = serverFactory.getWebServer(servletContext ->{
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-							).addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HellobootApplication.class);
-		
-		applicationContext.refresh();
+		SpringApplication.run(HellobootApplication.class,args);
 	}
+	
 }
