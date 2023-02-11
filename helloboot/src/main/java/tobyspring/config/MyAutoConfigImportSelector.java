d52ff155cf0a85ada59288256c86ca56bd56c57b
kks <kks@host.docker.internal>
2023-02-11 20:33:05 +0900

    자동구성정보파일 생성 읽기

--- helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,25 @@
 package tobyspring.config;
 
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+	
+	private final ClassLoader classLoader;
+	
+	public MyAutoConfigImportSelector(ClassLoader classLoader) {
+		super();
+		this.classLoader = classLoader;
+	}
 
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-				"tobyspring.config.autoconfig.DispatcherServletConfig",
-				"tobyspring.config.autoconfig.TomcatWebServerConfig"
-		};
+		Iterable<String> load = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+		return StreamSupport.stream(load.spliterator(), false).toArray(String[]::new);
 	}
 
 }
