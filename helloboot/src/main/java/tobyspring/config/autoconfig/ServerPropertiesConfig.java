0645976820485b89982e3c18ace314459d87d080
kks <kks@host.docker.internal>
2023-02-13 21:12:50 +0900

    프로퍼티 자동 바인딩 Binder 클래스

--- helloboot/src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
+++ helloboot/src/main/java/tobyspring/config/autoconfig/ServerPropertiesConfig.java
@@ -1,5 +1,6 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -10,9 +11,7 @@ import tobyspring.config.MyAutoConfiguration;
 public class ServerPropertiesConfig {
 	@Bean
 	public ServerProperties properties(Environment env) {
-		ServerProperties properties = new ServerProperties();
-		properties.setContextPath(env.getProperty("contextPath"));
-		properties.setPort(Integer.valueOf(env.getProperty("port")));
-		return properties;
+		//프로퍼티 파일을 읽어서 일일이 내가 만든 프로퍼티 객체에 바인딩 할 필요없이 자동화
+		return Binder.get(env).bind("", ServerProperties.class).get();
 	}
 }
